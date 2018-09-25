7416b603babba290756ed69ba555a014461ece76
Savannah Younts <savyounts@gmail.com>
2018-09-25 16:51:21 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user, reject_if: :reject_user
+  accepts_nested_attributes_for :user, reject_if: :all_blank
 
 
-  def reject_user
-    attributes[:username].blank?
-  end
+  # def reject_user
+  #   attributes[:username].blank?
+  # end
 
   def user_name
     self.user.username

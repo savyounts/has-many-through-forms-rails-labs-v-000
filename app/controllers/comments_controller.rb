f91122958ece43b949bffaddc99fcd0c744572a5
Savannah Younts <savyounts@gmail.com>
2018-09-25 16:32:29 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
 
   def create
     if !params[:comment][:user_id].empty?
-      @comment = Comment.create(comment_params(:content, :post_id, :user_id))
+      @comment = Comment.create(comment_params)
     else
       @user = User.create(username: params[:comment][:user_attributes][:username])
-      @comment = Comment.create(comment_params(:content, :user_attributes => [:username], :post_id)) 
+      @comment = Comment.create(comment_params) 
     end 
       redirect_to post_path(comment.post)
   end
@@ -27,6 +27,6 @@ class CommentsController < ApplicationController
   private
 
     def comment_params(*args)
-      params.require(:comment).permit(*args)
+      params.require(:comment).permit(:user_id, :content, :post_id, :user_attributes => [:username])
     end
 end

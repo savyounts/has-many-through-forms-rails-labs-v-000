462d1264b12bae5480a95f788a762f6d1ce953a3
Savannah Younts <savyounts@gmail.com>
2018-09-25 15:57:27 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,27 @@
 class CommentsController < ApplicationController
 
-  def create
-    if !params[:comment][:user_id].empty?
-      @user = User.find(params[:comment][:user_id])
-      @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id])
-    elsif params[:comment][:user_id].empty?
-      @user = User.create(username: params[:comment][:user_attributes][:username])
-      @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id]) 
-    end 
-      redirect_to post_path(comment.post)
-  end
-
-  # def create 
-  #   if !params[:comment][:user_id].empty? 
-  #     @user = User.find(params[:comment][:user_id]) 
-  #     @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id]) 
-  #   elsif 
-  #     params[:comment][:user_id].empty? 
-  #     @user = User.new(username: params[:comment][:user_attributes][:username]) 
-  #     @user.save 
+  # def create
+  #   if !params[:comment][:user_id].empty?
+  #     @user = User.find(params[:comment][:user_id])
+  #     @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id])
+  #   elsif params[:comment][:user_id].empty?
+  #     @user = User.create(username: params[:comment][:user_attributes][:username])
   #     @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id]) 
   #   end 
-  #   redirect_to post_path(params[:comment][:post_id])  
+  #     redirect_to post_path(comment.post)
   # end
+
+  def create 
+    if !params[:comment][:user_id].empty? 
+      @user = User.find(params[:comment][:user_id]) 
+      @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id]) 
+    elsif  params[:comment][:user_id].empty? 
+      @user = User.new(username: params[:comment][:user_attributes][:username]) 
+      @user.save 
+      @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id]) 
+    end 
+    redirect_to post_path(params[:comment][:post_id])  
+  end
 
   private
 

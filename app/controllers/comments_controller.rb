53164d06424c2c0a8a85a86c271ff6f3f98a1730
Savannah Younts <savyounts@gmail.com>
2018-09-25 16:08:14 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     if !params[:comment][:user_id].empty?
-      @user = User.find(params[:comment][:user_id])
-      @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id])
+      @comment = Comment.create(comment_params)
     elsif params[:comment][:user_id].empty?
       @user = User.create(username: params[:comment][:user_attributes][:username])
       @comment = Comment.create(content: params[:comment][:content], user_id: @user.id, post_id: params[:comment][:post_id]) 
